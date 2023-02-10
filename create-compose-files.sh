733a56276401bb8d0faefaa429366d2ab058afe1
David Cristóbal <david@juanked.es>
2023-02-10 19:21:13 +0100

    Raspberry version with arguments

--- create-compose-files.sh
+++ create-compose-files.sh
@@ -1,18 +1,49 @@
 #!/bin/sh
 
+Help()
+{
+  echo "Create compose files from running containers"
+  echo "Syntax: $0 [-h] backup-dir user-name"
+}
+
+Incorrect()
+{
+  echo "Incorrect usage, use -h for help"
+}
+
+while getopts ":h" option; do
+  case $option in
+    h)
+      Help
+      exit;;
+    *)
+      Incorrect
+      exit;;
+  esac
+done
+
+if [ $# -lt 2 ]; then
+    Incorrect
+    exit 1
+fi
+
 # Path to backup location
-backup_dir="$HOME/compose-backups"
+backup_dir="$1/compose-backups"
+user_name="$2"
 
 if [ ! -d "$backup_dir" ] ; then
   mkdir "$backup_dir"
   echo "Made backup dir at $backup_dir"
 fi
 
-docker pull red5d/docker-autocompose
+docker pull ghcr.io/red5d/docker-autocompose:latest
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-docker ps --format '{{.Names}}' > containers.txt
+docker ps --format '{{.Names}}' > "$backup_dir/containers.txt"
 while IFS="" read -r p || [ -n "$p" ]
 do
-  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock red5d/docker-autocompose $p > "$backup_dir/$p-$current_time.yaml"
-done < containers.txt
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/red5d/docker-autocompose $p > "$backup_dir/$p-$current_time.yaml"
+done < "$backup_dir/containers.txt"
+# chown "$user_name":"$user_name" "$backup_dir/containers.txt"
+chown -R "$user_name":"$user_name" "$backup_dir"
 find "$backup_dir" -name "*.yaml" -mtime +5 -exec rm {} \;
+
